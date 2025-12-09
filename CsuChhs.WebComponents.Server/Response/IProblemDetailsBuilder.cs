f992e8de7fca20ec1b4ecaa51777a3a77d2069e2
Derek Stegelman <dstegelman@gmail.com>
2025-12-09 14:11:39 -0700

    Add Problem details class some helpful builders

---
+++ CsuChhs.WebComponents.Server/Response/IProblemDetailsBuilder.cs
@@ -0,0 +1,11 @@
+﻿namespace CsuChhs.WebComponents.Server.Response;
+
+public interface IProblemDetailsBuilder
+{
+    IProblemDetailsBuilder WithStatus(int status);
+    IProblemDetailsBuilder WithTitle(string title);
+    IProblemDetailsBuilder WithDetail(string detail);
+    IProblemDetailsBuilder WithInstance(string instance);
+    IProblemDetailsBuilder WithType(string type);
+    ProblemDetails Build();
+}
---
+++ CsuChhs.WebComponents.Server/Response/ProblemDetailsBuilder.cs
@@ -0,0 +1,53 @@
+﻿namespace CsuChhs.WebComponents.Server.Response;
+
+public class ProblemDetailsBuilder : IProblemDetailsBuilder
+{
+    private string? _type;
+    private int? _status;
+    private string? _title;
+    private string? _detail;
+    private string? _instance;
+
+    public IProblemDetailsBuilder WithStatus(int status)
+    {
+        _status = status;
+        return this;
+    }
+
+    public IProblemDetailsBuilder WithTitle(string title)
+    {
+        _title = title;
+        return this;
+    }
+
+    public IProblemDetailsBuilder WithDetail(string detail)
+    {
+        _detail = detail;
+        return this;
+    }
+
+    public IProblemDetailsBuilder WithInstance(string instance)
+    {
+        _instance = instance;
+        return this;
+    }
+
+    public IProblemDetailsBuilder WithType(string type)
+    {
+        _type = type;
+        return this;
+    }
+
+    public ProblemDetails Build()
+    {
+        
+        return new ProblemDetails
+        {
+            Type = _type,
+            Status = _status,
+            Title = _title,
+            Detail = _detail,
+            Instance = _instance
+        };
+    }
+}
